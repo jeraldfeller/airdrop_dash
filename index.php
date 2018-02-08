cefec08ac40bba94f0947e981060987cabe249f6
Jerald Feller <jeraldfeller@gmail.com>
2018-02-08 23:48:24 +0800

    2018-02-08 Treasure Hunt

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ $privateKey = ($userData['hasShared'] == true ? $userData['keys']['private'][arr
     <meta charset="UTF-8" />
     <title>Main</title>
 
-    <link href="assets/css/style.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/style.css?v=1.2" rel="stylesheet" type="text/css">
     <link rel="icon" type="image/x-icon" href="" />
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
@@ -39,6 +39,21 @@ $privateKey = ($userData['hasShared'] == true ? $userData['keys']['private'][arr
 <?php include 'includes/regular/header.php'; ?>
 
 <div class="container-fluid currentpointsmargin" style="margin-top: 10%;">
+    <div style="float:left;width:100%;">
+        <div class="col-md-offset-4 col-md-4">
+            <div class="dashboardalert1">
+                <span></span> Please read the rules of the airdrop: <a href="https://medium.com/@ternio/ternio-100k-airdrop-8dbd49e28e52" target="_blank">AIRDROP RULES</a>
+            </div>
+
+            <div class="dashboardalert1">
+                <span></span> Join the Affiliate Program: <a href="https://airdrop.ternio.io/affiliate" target="_blank">AFFILIATE PROGRAM</a>
+            </div>
+
+            <div class="dashboardalert1">
+                <span></span> Earn Real Ternio Tokens: <a href="https://medium.com/@ternio/ternio-100k-airdrop-8dbd49e28e52" target="_blank">EARN REAL TOKENS!</a>
+            </div>
+        </div>
+    </div>
     <div class="row">
         <div class="col-md-12 text-center dash-currentpoint">
             <h2>Current Tokens: <?php echo $userData['points']; ?></h2>
@@ -60,13 +75,15 @@ $privateKey = ($userData['hasShared'] == true ? $userData['keys']['private'][arr
         <div class="row share-container">
             <div class="col-md-4 col-md-offset-4">
                 <div class="well well-sm sociallikewrap">
-                    <h3 class="text-center">Reveal Your Private Key</h3>
-                    <h4 class="text-center sociallike-bottomrevealtext">Share on one of the following social networks to reveal your private key:</h4>
+                    <h3 class="text-center">Reveal Your Confirmation Code</h3>
+                    <h4 class="text-center sociallike-bottomrevealtext">Share on one of the following social networks to reveal your confirmation code:</h4>
                     <div class="row">
                         <div class="col-md-12 text-center">
+                            <!--
                             <div class="col-md-4 col-sm-4 col-xs-4">
-                                <a style="cursor: pointer;" class="share-fb"><img src="assets/images/social/fb.png"></a>
-                            </div>
+                            <a style="cursor: pointer;" class="share-fb"><img src="assets/images/social/fb.png"></a>
+                        </div>
+                        -->
                             <div class="col-md-4 col-sm-4 col-xs-4">
                                 <a style="cursor: pointer;" class="share-twitter" ><img src="assets/images/social/twitter.png"></a>
 
@@ -108,14 +125,14 @@ $privateKey = ($userData['hasShared'] == true ? $userData['keys']['private'][arr
         }) ;
 
         // Twitter
-        $url = 'https://airdrop.ternio.io';
-        $text = 'enter in: bla bla bla';
+        $url = 'https://ternio.io';
+        $text = '#Ternio #blockchain is revolutionizing advertising @Digiday @Adweek <200f>@adage @TechCrunch @adexchanger @TheDrum @businessinsider @WSJ';
         $intent = 'https://twitter.com/intent/tweet?text='+encodeURIComponent($text)+'&url='+encodeURIComponent($url);
         $('.share-twitter').attr('href', $intent);
 
         // Reddit
-        $url = 'https://airdrop.ternio.io';
-        $title = 'bla bla bla';
+        $url = 'https://ternio.io';
+        $title = 'These guys are shaking up the way advertising works';
         $link = 'https://www.reddit.com/submit?url='+$url+'&title='+encodeURIComponent($title);
         $('.share-reddit').click(function(){
             window.open($link,'popup','width=800,height=800');
